b727dd9bd8013a03035ac7f57f8a60c30ce05053
MÓNICA RODRÍGUEZ CARO SÁNCHEZ <Monica.Rodriguez12@alu.uclm.es>
2025-10-01 17:47:29 +0200

    grid size

--- src/main/java/com/mygarden/app/models/Shop.java
+++ src/main/java/com/mygarden/app/models/Shop.java
@@ -9,10 +9,20 @@ public class Shop {
 
     public Shop() {
         ShopItems = new ArrayList<>();
-        ShopItems.add(new ShopItem("Tulip", 10, "/images/shopImg/tulip.png", 0));
-        ShopItems.add(new ShopItem("Rose", 20, "/images/shopImg/rose.png", 0));
-        ShopItems.add(new ShopItem("Sunflower", 30, "/images/shopImg/sunflower.png", 1));
-        ShopItems.add(new ShopItem("Daisy", 40, "/images/shopImg/daisy.png", 2));
+        ShopItems.add(new ShopItem("Tulip", 15, "/images/shopImg/tulip.png", 0));
+        ShopItems.add(new ShopItem("Rose", 25, "/images/shopImg/rose.png", 0));
+        ShopItems.add(new ShopItem("Sunflower", 20, "/images/shopImg/sunflower.png", 0));
+        ShopItems.add(new ShopItem("Daisy", 40, "/images/shopImg/daisy.png", 0));
+         
+        ShopItems.add(new ShopItem("Boxwood", 35, "/images/shopImg/boxwood.png", 1));
+        ShopItems.add(new ShopItem("Hydrangea", 30, "/images/shopImg/hydrangea.png", 1));
+        ShopItems.add(new ShopItem("Lavender", 35, "/images/shopImg/lavander.png", 1));
+        ShopItems.add(new ShopItem("Rhododendron", 30, "/images/shopImg/rhododendron.png", 1));
+
+        ShopItems.add(new ShopItem("Maple", 200, "/images/shopImg/maple.png", 2));
+        ShopItems.add(new ShopItem("Oak", 120, "/images/shopImg/oak.png", 2));
+        ShopItems.add(new ShopItem("Pine", 80, "/images/shopImg/pine.png", 2));
+        ShopItems.add(new ShopItem("Willow", 150, "/images/shopImg/willow.png", 2));
     }
 
     public ShopItem getShopItem(int i) {
